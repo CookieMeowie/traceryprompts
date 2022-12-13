3f9d98d7e9d92c2dcccdc439c13f01dc3c2dcd67
Stretch <ctwildgoose@gmail.com>
2022-12-13 22:32:24 +0000

    Now loads multiple json

--- scripts/traceryprompts.py
+++ scripts/traceryprompts.py
@@ -16,9 +16,15 @@ from modules.processing import Processed, process_images
 from modules.shared import cmd_opts, opts, state
 
 DEFAULT_RULES = "{\n\"origin\": \"#hello.capitalize#, #location#!\",\n\"hello\": [\"hello\", \"greetings\", \"howdy\", \"hey\"],\n\"location\": [\"world\", \"solor system\", \"galaxy\", \"universe\"]\n}"
-DEFAULT_PATH = "\\extensions\\traceryprompts\\rules.json"
+DEFAULT_RULES_PATH = "\\extensions\\traceryprompts\\mainrules.json"
+RULES_DIR_PATH = "\\extensions\\traceryprompts\\rules"
+
+
 
 class Script(scripts.Script):  
+
+    json_loaded:bool = False
+    rules_dict:dict = {}
 
     def load_json(jsonpath):
         # Load rules.json
@@ -42,41 +48,44 @@ class Script(scripts.Script):
 # Most UI components can return a value, such as a boolean for a checkbox.
 # The returned values are passed to the run method as parameters.
     def ui(self, is_img2img):
-        # Load json
-        with open(scripts.basedir() + DEFAULT_PATH) as data_file:
-            rules_dict = json.load(data_file)
+        if not self.json_loaded:
+            print("Ui calling load with " + str(len(self.rules_dict)) + " keys already loaded.")
+            self.load_json()
 
-        if len(rules_dict) == 0:
+        if len(self.rules_dict) == 0:
             print(f"\nInvalid grammar. No rules specified.")
             gr.Markdown("Error: Could not read rules.json!")
-            return [ showp ]
+            return [ True ]
         
         gr.Column()
         gr.Markdown("# Ultimate Randomizer of Ultimate Fun")
-        gr.Markdown("With Tracery Prompts as the active script you can use any of the lists below in place of tokens and they will be replaced, randomly at generation time.")
+        gr.Markdown("With Tracery Prompts as the active script you can use the key of any of the lists below in place of tokens and they will be randomly replaced at generation time.")
         gr.Markdown("## Example:")
         gr.Markdown(">best quality, 1girl, mature female, \#hair_colour\#, \#hairstyle\#, very long hair, \#iris_colour\#, wearing a white (\#dress\#:1.2) and #female_clothing#")
+        gr.Markdown("## Notes:")
+        gr.Markdown("- Check the prompt in your final image. If you see any of the keys in the promt surrounded by double parenthesis like ((hair_color)), then that key was not recognised and you probably spelt it wrong or forgot to include surrounding \# \n- These dropdowns don't do anything. They're just here to help you reference the keys and explore the lists for ideas.")
+        gr.Markdown("")
         gr.Markdown("## Reference:")
 
-        keys = list(rules_dict.keys())
+        keys = list(self.rules_dict.keys())
         # order
         keys.sort()
         i = 0
         while True:
             with gr.Row():
-                gr.Dropdown(rules_dict[keys[i]], label=keys[i])
+                gr.Dropdown(self.rules_dict[keys[i]], label=keys[i])
                 i += 1
                 if i >= len(keys):
                     break
-                gr.Dropdown(rules_dict[keys[i]], label=keys[i])
+                gr.Dropdown(self.rules_dict[keys[i]], label=keys[i])
                 i += 1
                 if i >= len(keys):
                     break
-                gr.Dropdown(rules_dict[keys[i]], label=keys[i])
+                gr.Dropdown(self.rules_dict[keys[i]], label=keys[i])
                 i += 1
                 if i >= len(keys):
                     break
-                gr.Dropdown(rules_dict[keys[i]], label=keys[i])
+                gr.Dropdown(self.rules_dict[keys[i]], label=keys[i])
                 i += 1
                 if i >= len(keys):
                     break
@@ -94,19 +103,11 @@ class Script(scripts.Script):
 # to be used in processing. The return value should be a Processed object, which is
 # what is returned by the process_images method.
     def run(self, p, showp):
-        # Load rules.json
-        with open(scripts.basedir() + DEFAULT_PATH) as data_file:
-            rules_dict = json.load(data_file)
-
-        if len(rules_dict) == 0:
-            print(f"\nInvalid grammar. No rules specified.")
-            return
-
         # Add main prompt in as main
-        rules_dict["main"] = p.prompt
+        self.rules_dict["main"] = p.prompt
 
         # Finalise tracery
-        grammar = tracery.Grammar(rules_dict)
+        grammar = tracery.Grammar(self.rules_dict)
 
         # Prep for iterating
         BatchSize = p.batch_size
@@ -135,3 +136,32 @@ class Script(scripts.Script):
         p.n_iter = IterCount
 
         return Processed(p=p, images_list=images, info=infotexts[0], infotexts=infotexts)
+    
+    def load_json(self):
+        self.json_loaded = True
+
+        # Load main rules.json
+        with open(scripts.basedir() + DEFAULT_RULES_PATH) as data_file:
+            main_rules:dict = json.load(data_file)
+        
+        # Discover json files in the rules directory
+        files = []
+        for file in os.listdir(scripts.basedir() + RULES_DIR_PATH):
+            if file.endswith(".json"):
+                files.append(file)
+        
+        # Load each json
+        new_rules:dict = {}
+        for f in files:
+            with open(scripts.basedir() + RULES_DIR_PATH + "\\" + f) as data_file:
+                new_dict:dict = new_rules.copy()
+                new_dict.update(json.load(data_file))
+                new_rules = new_dict
+        
+        final_rules:dict = main_rules.copy()
+        final_rules.update(new_rules)
+        self.rules_dict = final_rules
+
+        
+        print("Done. Loaded: " + str(len(self.rules_dict)) + " entries.")
+        
